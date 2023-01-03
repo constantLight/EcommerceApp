3521543cee300b0144929eba5da463bb1decfb77
Karthik <karthiknaidu@Karthiks-MBP.Dlink>
2023-01-03 21:52:34 +0530

    NoSQL with MongoDB Connections

--- util/database.js
+++ util/database.js
@@ -1,5 +1,29 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node_schema', 'root', 'Kn_recon@18', {dialect: 'mysql', host: 'localhost'});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://KarthikRamana:1HFvpem9avW2Fnw1@cluster0.ip8sdwo.mongodb.net/?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
