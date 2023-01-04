37b4b34599e880fe2975cb13e5883a82eba2362c
Karthik <karthiknaidu@Karthiks-MBP.Dlink>
2023-01-04 18:25:28 +0530

    Implementing Actions Using Mongoose

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -19,9 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('63a9bb4b38f138328d2dc632')
+  User.findById('63b53c3b7be11c60089d399d')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch(err => console.log(err));
@@ -32,6 +32,20 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose.connect('mongodb+srv://KarthikRamana:1HFvpem9avW2Fnw1@cluster0.ip8sdwo.mongodb.net/shop?retryWrites=true&w=majority').then(result => {
+  User.findOne().then(user => {
+    if(!user){
+      const user = new User({
+        name: 'Karthik',
+        email: 'test@test.com',
+        cart: {
+          items: []
+        }
+      });
+      user.save();
+    }
+  })
   app.listen(3000);
-});
+}).catch(err => {
+  console.log(err);
+})
